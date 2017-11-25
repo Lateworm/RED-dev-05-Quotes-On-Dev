71698b93e3a71d9aacabf2ded418d740986d58d2
Late Worm <travis.rempel@gmail.com>
2017-11-25 13:24:40 -0800

    Open RED link in a new window.

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-					Brought to you by<a href="https://redacademy.com">&nbsp; RED Academy</a></span>
+					Brought to you by<a href="https://redacademy.com" target="_blank">&nbsp; RED Academy</a></span>
 				</nav>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
